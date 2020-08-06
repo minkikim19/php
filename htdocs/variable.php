ddb74ec785ff9739385e85ab993f613f7e15653f
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-07 01:27:39 +0900

    Add files via upload
    up

---
+++ htdocs/variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+        $name = "kim";
+        echo "asdjasd ".$name." asdjkh kasj dj askjh dasjk hdjkwe hdjksfh sjkdh fjksdh
+         fkjsh jksdfh jksdh kim fjksdh fkhsduif hsdhf askhd jksah ifushduihwe
+         sdhk"
+        ?>
+    </body>
+</html>
