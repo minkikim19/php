0b98b66ab1af9e21ca5835d11d2efd09675eaf6e
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-07 18:29:21 +0900

    Add files via upload
    php url parameter

---
+++ htdocs/parameter.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+    안녕하세요 <?php echo $_GET['name']; ?>님 <?php echo $_GET['address']; ?>의 가장 가까운 경로를 설정해 드리겠습니다.    
+    </body>
+</html>
