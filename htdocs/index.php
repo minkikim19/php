c6e7cb335d3007af43944635a5fb0b3b62aadd96
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-07 18:39:12 +0900

    Add files via upload
    Use of url parameter

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
-<html>
-  <body>
-<?php
-    echo date('Y-m-d H:i:s');
-?>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <?php 
+    echo $_GET['id'];
+    ?>
+    <h2>ksadj laksj dlkasj weiofj jea fjdiosdj fioej sdio</h2>
+</body>
 </html>
