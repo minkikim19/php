b26dcfed73093694db7fc25fca3737c54ac96c26
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-08 11:46:58 +0900

    Add files via upload
    Conditional

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,19 +6,35 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
         <li><a href="index.php?id=JavaScript">JavaScript</a></li>
     </ol>
-    <?php 
-    echo $_GET['id'];
-    ?>
     <h2>
         <?php 
-        echo file_get_contents("data/".$_GET['id']);
+            if(isset($_GET['id']))
+            {
+                echo $_GET['id'];
+            }
+            else
+            {
+                echo "Welcom";
+            }
         ?>
     </h2>
+    <?php 
+        if(isset($_GET['id']))
+        {
+            echo file_get_contents("data/".$_GET['id']);
+        }
+        else
+        {
+            echo "About the web!";
+        }        
+    ?>
+
+        
 </body>
 </html>
