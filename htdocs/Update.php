125bdc33e2da624d2acaed9824c13b6f6117b63c
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-09 23:20:28 +0900

    Add files via upload
    Implemented post editing function

--- htdocs/Update.php
+++ htdocs/Update.php
@@ -62,13 +62,17 @@ function print_title()
       <a href="Update.php?id=<?=$_GET['id'];?>">Update</a>
     <?php 
     } ?>
-    <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_description();
-    ?>
+    <form action="Update_process.php" method="post">
+    <input type="hidden" name="old_title" value="<?=$_GET['id']?>">
+    <p>
+        <input type="text" name="title" placeholder="Title" value="<?php print_title(); ?>">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"><?php print_description(); ?></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
   </body>
 </html>
