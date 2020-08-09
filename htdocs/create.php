afcafd35ce18d13565cf6289058ad3c95da6b166
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-09 19:16:37 +0900

    Add files via upload
    Create article

--- htdocs/create.php
+++ htdocs/create.php
@@ -56,13 +56,16 @@ function print_title()
       ?>
     </ol>
     <a href="Create.php">Create</a>
-    <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_description();
-    ?>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
   </body>
 </html>
